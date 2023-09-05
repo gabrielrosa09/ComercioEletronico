df63d1a9d2e91889e6a8616f143da8f7cb810761
Gabriel da Silva Rosa <gabriel10919@outlook.com>
2023-09-04 22:27:10 -0300

    subindo database

--- todos/models.py
+++ todos/models.py
@@ -1,6 +1,46 @@
 from django.db import models
 
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=255)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    # Outros campos de perfil, como nome, endereço, número de telefone, etc.
 
-# Create your models here.
-class Todo(models.Model):
-    pass
+class Merchant(models.Model):
+    merchant_id = models.AutoField(primary_key=True)
+    merchant_name = models.CharField(max_length=255)
+    merchant_description = models.TextField()
+    merchant_logo = models.URLField()
+    # Outros detalhes do comerciante
+
+class Product(models.Model):
+    product_id = models.AutoField(primary_key=True)
+    product_name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.IntegerField()
+    merchant = models.ForeignKey(Merchant, on_delete=models.CASCADE)
+    # Outros campos relacionados ao produto
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    total_price = models.DecimalField(max_digits=10, decimal_places=2)
+    # Outros detalhes do pedido
+
+class OrderItem(models.Model):
+    item_id = models.AutoField(primary_key=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    subtotal_price = models.DecimalField(max_digits=10, decimal_places=2)
+
+class PaymentMethod(models.Model):
+    payment_method_id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cardholder_name = models.CharField(max_length=255)
+    card_number = models.CharField(max_length=16)
+    expiration_date = models.DateField()
+    billing_address = models.TextField()
